0c64fd59d5b7c22fb931becffd7942ce6f85b301
Gábor L. Hajba <gabor.hajba@gmail.com>
2025-12-01 13:42:33 +0100

    Day 2 part 2

--- java/day01/Day01.java
+++ java/day01/Day01.java
@@ -1,28 +1,40 @@
 void main(String[] args) {
-    File file = new File("java/day01/day01.txt");
+    File file = new File("input/day01/day01.txt");
     try (FileReader fileReader = new FileReader(file)) {
         int zeros = 0;
+        int passingOverZero = 0;
         int current = 50;
+
         for (String line : fileReader.readAllLines()) {
-            int turns = Integer.parseInt(line.substring(1)) % 100;
+            int toNextZero;
+            int direction;
+            int turns = Integer.parseInt(line.substring(1));
+
             if (line.startsWith("L")) {
-                current -= turns;
-                if (current < 0) {
-                    current = current + 100;
-                }
+                toNextZero = current == 0 ? 100 : current;
+                direction = -1;
             } else {
-                current += turns;
-                if (current >= 100) {
-                    current = current - 100;
-                }
+                toNextZero = 100 - current;
+                direction = 1;
+            }
+
+            current = (current + direction * turns) % 100;
+            if (current < 0) {
+                current += 100;
             }
 
             if (current == 0) {
                 zeros++;
             }
-            System.out.printf("%s --> %d%n", line, current);
+
+            if (turns >= toNextZero) {
+                passingOverZero += (turns - toNextZero) / 100 + 1;
+            }
+
+            //            System.out.printf("%s --> %d%n", line, current);
         }
-        System.out.println("Zeros: " + zeros);
+        System.out.println("Zeros: " + zeros); // 1086
+        System.out.println("Passing zero: " + passingOverZero); // 6268
     } catch (IOException e) {
         throw new RuntimeException(e);
     }
